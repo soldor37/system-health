c25d3483da7e15e78a3ae3da1e0ecad211de611e
George Lubenets <seventhpirate@gmail.com>
2021-03-13 08:25:45 +0300

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify';
+import VueApexCharts from 'vue-apexcharts'
+
+Vue.use(VueApexCharts)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
